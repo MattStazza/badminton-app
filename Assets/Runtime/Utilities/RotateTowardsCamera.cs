79bc93ba1a5239406b5ae4cbb339f511b46192fe
MattStazza <63791313+MattStazza@users.noreply.github.com>
2024-09-28 22:44:56 +0800

    Added Service Indicator

--- Assets/Runtime/Utilities/RotateTowardsCamera.cs
+++ Assets/Runtime/Utilities/RotateTowardsCamera.cs
@@ -5,6 +5,7 @@ namespace Runtime.Utilities
     public class RotateTowardsCamera : MonoBehaviour
     {
         [SerializeField] private float rotateSpeed = 2f;
+        [SerializeField] private Vector3 rotationAxis = new Vector3(1,1,1);
 
         private Transform cameraTransform;
 
@@ -20,6 +21,11 @@ namespace Runtime.Utilities
             if (directionToCamera != Vector3.zero)
             {
                 Quaternion targetRotation = Quaternion.LookRotation(directionToCamera);
+
+                targetRotation.x = targetRotation.x * rotationAxis.x;
+                targetRotation.y = targetRotation.y * rotationAxis.y;
+                targetRotation.z = targetRotation.z * rotationAxis.z;
+
                 transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotateSpeed);
             }
         }
