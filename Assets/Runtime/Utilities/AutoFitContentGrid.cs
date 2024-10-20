dab5b3e2c88b83834354e82abf6f2c6142379494
MattStazza <63791313+MattStazza@users.noreply.github.com>
2024-10-20 17:50:58 +0800

    Created Session Results Page

---
+++ Assets/Runtime/Utilities/AutoFitContentGrid.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runtime.Utilities
+{
+    public class AutoFitContentGrid : MonoBehaviour
+    {
+        [SerializeField] private RectTransform contentRectTransform;
+        [SerializeField] private GridLayoutGroup gridLayout;
+
+        private void Awake() => ValidateRequiredVariables();
+
+
+
+        private void OnEnable() => UpdateContentHeight();
+
+        private void FixedUpdate() => UpdateContentHeight();
+
+
+
+        private void UpdateContentHeight()
+        {
+            float contentHeight = 0;
+
+            if (gridLayout != null)
+            {
+                contentHeight = (((gridLayout.spacing.y + gridLayout.cellSize.y) * transform.childCount) / 4) + gridLayout.cellSize.y;
+            }
+
+            contentRectTransform.sizeDelta = new Vector2(contentRectTransform.sizeDelta.x, contentHeight);
+
+            RefreshContentLayout();
+        }
+
+        private void RefreshContentLayout()
+        {
+            gridLayout.enabled = false;
+            gridLayout.enabled = true;
+        }
+
+
+
+        private void ValidateRequiredVariables()
+        {
+            if (contentRectTransform == null) { Debug.LogError("Null References: " + contentRectTransform.name); }
+            if (gridLayout == null) { Debug.LogError("Null References: " + gridLayout.name); }
+        }
+    }
+}
--- Assets/Runtime/Utilities/AutoFitContentVetical.cs
+++ Assets/Runtime/Utilities/AutoFitContentVetical.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 
 namespace Runtime.Utilities
 {
-    public class SizeScrollAreaToFitContent : MonoBehaviour
+    public class AutoFitContentVetical : MonoBehaviour
     {
         [SerializeField] private RectTransform contentRectTransform;
         [SerializeField] private VerticalLayoutGroup verticalLayout;
-        [SerializeField] private GridLayoutGroup gridLayout;
 
         private void Awake() => ValidateRequiredVariables();
 
+
+
         private void OnEnable() => UpdateContentHeight();
 
-        private void FixedUpdate()
-        {
-            UpdateContentHeight();
-        }
+        private void FixedUpdate() => UpdateContentHeight();
+
+
 
         private void UpdateContentHeight()
         {
@@ -33,14 +33,7 @@ namespace Runtime.Utilities
 
                 contentHeight = childrenHeight + (verticalLayout.spacing * transform.childCount);
             }
-               
-
-            if (gridLayout != null)
-            {
-                contentHeight = (((gridLayout.spacing.y + gridLayout.cellSize.y) * transform.childCount) / 4) + gridLayout.cellSize.y;
-            }
-
-
+              
             contentRectTransform.sizeDelta = new Vector2(contentRectTransform.sizeDelta.x, contentHeight);
 
             RefreshContentLayout();
@@ -48,17 +41,8 @@ namespace Runtime.Utilities
 
         private void RefreshContentLayout()
         {
-            if (verticalLayout != null)
-            {
-                verticalLayout.enabled = false;
-                verticalLayout.enabled = true;
-            }
-
-            if (gridLayout != null)
-            {
-                gridLayout.enabled = false;
-                gridLayout.enabled = true;
-            }
+            verticalLayout.enabled = false;
+            verticalLayout.enabled = true;
         }
 
 
@@ -66,6 +50,7 @@ namespace Runtime.Utilities
         private void ValidateRequiredVariables()
         {
             if (contentRectTransform == null) { Debug.LogError("Null References: " + contentRectTransform.name); }
+            if (verticalLayout == null) { Debug.LogError("Null References: " + verticalLayout.name); }
         }
     }
 }
